7dc71efbb7ecc6aebe6d8bf9c8df5b27594cd8d4
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2020-08-10 15:35:41 +0200

    desyshowerloop

--- shower_sims/desyshowerloop.C
+++ shower_sims/desyshowerloop.C
@@ -32,29 +32,64 @@ void desyshowerloop(TString filename = "ship.conical.PG_11-TGeant4.root"){
  int nelectrons[nplates];
  int maxelectrons;
  int maxplate;
+
+ TVector3 vertexpos, hitpos;
+ TVector3 startP;  
+ double tx,ty;
+
+ const int nangles = 20;
+
+ double displacement_angle;
+ double coneangles[nangles] = {0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.10,0.11,0.12,0.13,0.14,0.15,0.16,0.17,0.18,0.19,0.20};
+ int nincone[nangles] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //n particles in cone (for all showers)
+ int nelectronstot = 0;
+
+ TGraph *effgraph = new TGraph();
+
  for(int ievent = 0; ievent<nevents;ievent++){
      for(int iplate =0; iplate < nplates; iplate++){
       nelectrons[iplate] = 0; //resetting number of electrons;
      }
 
      reader.SetEntry(ievent);
+     //vertex track position and angles
+     vertexpos.SetXYZ(tracks[0].GetStartX(),tracks[0].GetStartY(),tracks[0].GetStartZ());
+     startP.SetXYZ(tracks[0].GetPx(), tracks[0].GetPy(), tracks[0].GetPz());
+   
      //access the hits:    
      for (const EmuDESYPoint& emupoint: emupoints){
          //pdgcode and kinematics
+
          pdgcode = emupoint.PdgCode();
+
          momentum = TMath::Sqrt(pow(emupoint.GetPx(),2) + pow(emupoint.GetPy(),2) + pow(emupoint.GetPz(),2));
+         tx = emupoint.GetPx()/emupoint.GetPz();
+         ty = emupoint.GetPy()/emupoint.GetPz();
+ 
          plateid = emupoint.GetDetectorID();
+         hitpos.SetXYZ(emupoint.GetX(), emupoint.GetY(), emupoint.GetZ());
+ 
+         TVector3 displacement = hitpos - vertexpos; //vettore spostamento;         
+         displacement_angle = displacement.Angle(startP);
+
          //checking particle information
          if(pdg->GetParticle(pdgcode)){
           charge = pdg->GetParticle(pdgcode)->Charge();          
           mass = pdg->GetParticle(pdgcode)->Mass();         
-          //saving info for electrons and positrons
-          if (TMath::Abs(charge)>0){
-           energy = TMath::Sqrt(mass*mass + momentum * momentum);
+          //saving info for electrons and positrons above visibility threshold
+          if (TMath::Abs(charge)>0 && momentum > 0.03 && TMath::ATan(TMath::Sqrt(tx*tx+ty*ty))<1.){
+          
+           for(int iangle = 0; iangle < nangles; iangle++){ //is hit inside cone?
+            if(TMath::Abs(displacement_angle) < coneangles[iangle]) nincone[iangle]++;
+           }  
+           nelectronstot++;
            
+
+           energy = TMath::Sqrt(mass*mass + momentum * momentum);         
            hE_pid->Fill(plateid, energy);
            hoccupancy->Fill(plateid);
            nelectrons[plateid-1]++;
+
           }
          }
      }//end of hit loop
@@ -69,13 +104,26 @@ void desyshowerloop(TString filename = "ship.conical.PG_11-TGeant4.root"){
   hmaxsegments->Fill(maxelectrons);
  } //end of event loop 
 
+ for(int iangle = 0; iangle < nangles; iangle++){
+  effgraph->SetPoint(iangle, coneangles[iangle], (double) nincone[iangle]/nelectronstot);
+ }
+ 
+ effgraph->SetTitle("Cone angle acceptance;#Theta[rad];eff");
+ effgraph->GetYaxis()->SetRangeUser(0.,1.);
+ effgraph->SetMarkerColor(kRed);
+ effgraph->SetMarkerStyle(8);
+ effgraph->Draw("AP");
+
+ 
+ TCanvas *cE = new TCanvas();
+
  hE_pid->Draw();
  TCanvas *c_occ = new TCanvas();
  hoccupancy->Scale(1./nevents);
  hoccupancy->Draw();
  
  TCanvas *cmax = new TCanvas();
- cmax->Divide(1,2);
+ cmax->Divide(2,1);
  cmax->cd(1);
  hmaxplate->Draw();
  cmax->cd(2);
