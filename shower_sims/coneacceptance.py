dbb80f57b3b1a8f997861ddb86336ac3093c1ccb
Antonio Iuliano <antonio.iuliano1993@gmail.com>
2020-09-09 11:18:35 +0200

    cone acceptance modification

--- shower_sims/coneacceptance.py
+++ shower_sims/coneacceptance.py
@@ -1,11 +1,11 @@
 from __future__ import division
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import ROOT as r
 
 simdf = pd.read_csv("RUN3.csv")
 
-def doevent(mcevent,df, alphamax, Rmax):
+def doevent(mcevent,df, alphalist, Rlist, nbins):
     '''acceptance and purity for event mcevent in dataframe df'''
     firstsegment = df.query("PID==28 and  MCTrack==0 and MCEvent=={}".format(mcevent))
 
@@ -27,37 +27,61 @@ def doevent(mcevent,df, alphamax, Rmax):
     #cone angle
     df["alpha"] = np.arctan(np.sqrt(np.power(df["dx"]/df["dz"],2)+np.power(df["dy"]/df["dz"],2)))
 
-    #how many within conditions?
-    recodf = df.query("dR < {} and alpha < {}".format(Rmax,alphamax))
+    #how many within conditions, loop on dr and alpha? For each couple, I compute efficiency and purities.
+    istep = 0
+    efficiency = np.zeros(nbins * nbins)
+    purity = np.zeros(nbins * nbins)
+    for Rmax in Rlist:
+        for alphamax in alphalist:
+            recodf = df.query("dR < {} and alpha < {}".format(Rmax,alphamax))
+            Ntruesim = len(simdf.query("MCEvent=={}".format(mcevent)))
+            Ntruereco = len(recodf.query("MCEvent=={}".format(mcevent)))
+            Nallreco = len(recodf)
 
-    Ntruesim = len(simdf.query("MCEvent=={}".format(mcevent)))
-    Ntruereco = len(recodf.query("MCEvent=={}".format(mcevent)))
-    Nallreco = len(recodf)
+            efficiency[istep] = Ntruereco / Ntruesim
+            purity[istep] = Ntruereco / Nallreco
 
-    efficiency = Ntruereco / Ntruesim
-    purity = Ntruereco / Nallreco
+            istep = istep + 1
 
     return (efficiency, purity)
 
 nevents = 360
 
-alphamax = 0.04
-rmax = 800 #microns
+nbins = 61
+alphalist = np.linspace(0.02,0.08,nbins)
+Rlist = np.linspace(200,2000,nbins) #microns
 
-allefficiencies = []
-allpurities = []
+allefficiencies = np.zeros(nbins * nbins)
+allpurities = np.zeros(nbins*nbins)
+
 for ievent in range(nevents):
- (efficiency, purity) = doevent(ievent,simdf,alphamax, rmax)
- allefficiencies.append(efficiency)
- allpurities.append(purity)
+ print("arrived at event ", ievent)
+ (efficiency, purity) = doevent(ievent,simdf,alphalist, Rlist, nbins)
+ allefficiencies = allefficiencies + efficiency
+ allpurities = allpurities + purity
+allefficiencies = allefficiencies/nevents
+allpurities = allpurities/nevents
+#plotting result efficiency and purity maps
+effgraph = r.TGraph2D()
+puritygraph = r.TGraph2D()
+point = 0
+for Rpoint in range(nbins):
+    for alphapoint in range(nbins):
+     effgraph.SetPoint(point, alphalist[alphapoint], Rlist[Rpoint], allefficiencies[point])
+     puritygraph.SetPoint(point, alphalist[alphapoint], Rlist[Rpoint], allpurities[point])
+     point = point + 1
+c0 = r.TCanvas()
+#My2DGraph->SetMarkerStyle(21); // style 21 = "full square" My2DGraph->SetMarkerSize(1.0); // size 1.0 = 8 pixels on the screen My2DGraph->Draw("PCOL"); gPad->Modified(); gPad->Update(); /
 
-#plotting result histograms
-fig1 = plt.figure()
-plt.hist(allefficiencies, range = [0,1], bins = 10)
-plt.xlabel("efficiency")
+effgraph.SetTitle("Efficiency in function of cone aperture angle and cylinder radius;maximum aperture angle [rad]; maximum radius [#mu m]")
+#effgraph.SetMarkerStyle(21)
+#effgraph.SetMarkerSize(1.0)
+#effgraph.Draw("PCOL")
+effgraph.Draw("COLZ")
 
-fig2 = plt.figure()
-plt.hist(allpurities, range = [0,1], bins = 10)
-plt.xlabel("purity")
-
-plt.show()
+c1 = r.TCanvas()
+puritygraph.SetTitle("Purity in function of cone aperture angle and cylinder radius;maximum aperture angle [rad]; maximum radius [#mu m]")
+#puritygraph.SetMarkerStyle(21)
+#puritygraph.SetMarkerSize(1.0)
+#puritygraph.Draw("PCOL")
+puritygraph.Draw("COLZ")
