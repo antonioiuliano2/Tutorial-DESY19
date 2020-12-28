3e473dbb1b6adafd0a507374dbb474ca39f23a42
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2020-12-28 09:16:23 +0100

    adding env variables and shower reconstruction command

--- bash/doreconstruction.sh
+++ bash/doreconstruction.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-
+#performing all reconstruction, FairShip2Fedra.rootrc and showerrec.rootrc need to be set in folder
+NRUN=7 
 #RUNFOLDER=7GeV
 #cd $RUNFOLDER
 #copying rootrc and making folders to store couples
-cp $HOME/Lavoro/Tutorial-DESY19/FairShip2FEDRA/FairShip2Fedra.rootrc ./
-mkdir b000003
-mkdir b000003/p00{1..9}
-mkdir b000003/p0{10..29}
+mkdir b00000$NRUN
+mkdir b00000$NRUN/p00{1..9}
+mkdir b00000$NRUN/p0{10..29}
+
+cp showerrec.rootrc b00000$NRUN/
 #performing conversion
-root -l $HOME/Lavoro/Tutorial-DESY19/FairShip2FEDRA/fromFairShip2Fedra.C
+root -l -q $DESYMACROS/FairShip2FEDRA/fromFairShip2Fedra.C
 
 #going in reconstruction folder and copying required files for track reconstruction
-cd b000003
-cp $HOME/Lavoro/Tutorial-DESY19/FEDRA/scanset.sh ./
-cp $HOME/Lavoro/Tutorial-DESY19/FEDRA/track.rootrc ./
+cd b00000$NRUN
+echo "makescanset -set="$NRUN".0.0.0 -suff=cp.root -dz=-1315 -from_plate=29 -to_plate=1">scanset.sh
+cp $DESYMACROS/FEDRA/track.rootrc ./
 
 source scanset.sh
-emtra -set=3.0.0.0 -new -v=2
+emtra -set=$NRUN.0.0.0 -new -v=2
 #preparing final csv
 
-python $HOME/Lavoro/Tutorial-DESY19/scipy_conversion/csvconversion.py 3
+python $DESYMACROS/scipy_conversion/csvconversion.py $NRUN
+
+ln -s b00000$NRUN.0.0.0.trk.root linked_tracks.root
+root -l -q $DESYMACROS/shower_reconstruction/shower_reconstruction.C
