e7a351200a827c01ecb2e193a4b362e1ca0574de
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2021-01-05 13:02:45 +0100

    consider only central linearity region

--- shower_reconstruction/energycalibration.C
+++ shower_reconstruction/energycalibration.C
@@ -1,13 +1,13 @@
 //check molteplicity of reconstructed sim showers
 void energycalibration(){
 
- const int nenergies = 7;
+ const int nenergies = 10;
  float energies[nenergies] = {1,2,3,4,5,6,7};
  const float epsilon = 0.1;
 
  TString prepath_fit = "/eos/user/a/aiuliano/public/sims_FairShip/sim_DESY19/runs_360events/RUN3_differentenergies_17_December_2020/";
  TString prepath_res = "/eos/user/a/aiuliano/public/sims_FairShip/sim_DESY19/runs_360events/RUN3_differentenergiestest_18_December_2020/";
- TString foldernames[nenergies] = {"1GeV","2GeV","3GeV","4GeV","5GeV","6GeV","7GeV"};
+ TString foldernames[nenergies] = {"1GeV","2GeV","3GeV","4GeV","5GeV","6GeV","7GeV","8GeV","9GeV","10GeV"};
  TString subpath("/b000003/shower1.root");
 
  TChain *showerchain = new TChain("treebranch");
@@ -19,6 +19,8 @@ void energycalibration(){
  int maxsize = 1000;
  int sizeb;
  float P[maxsize];
+ float emass = 0.00051;
+ float showerenergy;
 
  showerchain->SetBranchAddress("sizeb",&sizeb);
  showerchain->SetBranchAddress("ntrace3simub",&P);
@@ -31,10 +33,10 @@ for (int ienergy = 0; ienergy < nenergies; ienergy++){
  }
  
  //profile histogram
- TProfile *shower_Esize = new TProfile("shower_Esize","Calibration of shower energy measurement;P[GeV/c];sizeb",80,0,8,0,200);
- TProfile *shower_sizeE = new TProfile("shower_sizeE","Calibration of shower energy measurement;sizeb;P[GeV/c]",20,0,200,0,8);
- TH1D *hres = new TH1D("hres", "Momentum resolution;#DeltaP/P",20,-1,1);
- TH2D *hPtruePrec = new TH2D("hPtruePrec","Prec vs Ptrue;Ptrue[GeV/c];Prec[GeV/c]", 80,0,8,80,0,8);
+ TProfile *shower_Esize = new TProfile("shower_Esize","Calibration of shower energy measurement;E[GeV];sizeb",100,0,10,0,300);
+ TProfile *shower_sizeE = new TProfile("shower_sizeE","Calibration of shower energy measurement;sizeb;E[GeV]",30,0,300,0,10);
+ TH1D *hres = new TH1D("hres","Energy resolution;#DeltaE/E",10,-1.05,0.95);
+ TH2D *hEtrueErec = new TH2D("hEtrueErec","Erec vs Etrue;Etrue[GeV/c];Erec[GeV/c]", 100,0,10,100,0,10);
  
  //starting loop
 
@@ -42,8 +44,9 @@ for (int ienergy = 0; ienergy < nenergies; ienergy++){
   showerchain->GetEntry(ishower);
   float P_MC = energies[showerchain->GetTreeNumber()]; //true simulated P
   if (P[0] > P_MC - epsilon && sizeb > 29) {
-   shower_Esize->Fill(P[0],sizeb);
-   shower_sizeE->Fill(sizeb,P[0]);
+   showerenergy = TMath::Sqrt(P[0] * P[0] + emass * emass);
+   shower_Esize->Fill(showerenergy,sizeb);
+   shower_sizeE->Fill(sizeb,showerenergy);
   }
  }
 
@@ -58,11 +61,11 @@ for (int ienergy = 0; ienergy < nenergies; ienergy++){
  TF1 *calfunc = new TF1("calfunc","pol1",30,200);
  TCanvas *c1 = new TCanvas();
  shower_sizeE->Draw();
- shower_sizeE->Fit(calfunc,"","",30,200);
+ shower_sizeE->Fit(calfunc,"","",50,180);
 
  float intercept = calfunc->GetParameter(0);
  float slope = calfunc->GetParameter(1);
- float Prec, Pres;
+ float Erec, Eres;
 
  const int nentriesres = showerchain->GetEntries() - nentriesfit;
  cout<<"TOTAL ENTRIES FOR ESTIMATION OF RESOLUTION"<<nentriesres<<endl;
@@ -71,12 +74,14 @@ for (int ienergy = 0; ienergy < nenergies; ienergy++){
 
  for (int ishower = nentriesfit; ishower < (nentriesfit+nentriesres);ishower++){
   showerchain->GetEntry(ishower);
-  if (sizeb > 29) {
-   Prec = sizeb * slope + intercept;
-   Pres = (Prec - P[0])/P[0];
+  if (sizeb > 50 && sizeb < 180) {
+   showerenergy = TMath::Sqrt(P[0]*P[0] + emass * emass);
 
-   hres->Fill(Pres);
-   hPtruePrec->Fill(P[0],Prec);
+   Erec = sizeb * slope + intercept;
+   Eres = (Erec - showerenergy)/showerenergy;
+
+   hres->Fill(Eres);
+   hEtrueErec->Fill(showerenergy,Erec);
   }
  }
 
@@ -86,6 +91,6 @@ for (int ienergy = 0; ienergy < nenergies; ienergy++){
  hres->Fit("gaus");
 
  TCanvas *c2D = new TCanvas();
- hPtruePrec->Draw("COLZ");
+ hEtrueErec->Draw("COLZ");
 
 }
