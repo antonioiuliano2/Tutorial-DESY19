55c0fb4c0bf0db3e9de85ba07c8c3d0be35aa1c2
Antonio Iuliano <antonio.iuliano1993@gmail.com>
2021-03-08 14:53:15 +0100

    comparison between standard and Random Forest shower reconstruction

---
+++ Dataset_preparation_ML_analysis/Lettura_csv/Make_histograms_data.py
@@ -0,0 +1,46 @@
+'''The other codes in Lettura CSV require true MC info, I am writing this one to produce histograms from data csv'''
+
+import pandas as pd
+import ROOT as r
+import root_numpy as rp
+
+minsegments = 50
+histofile = r.TFile("/home/utente/Lavoro/DE19_R3/RUN3_RandomForest_histos.root","RECREATE")
+#importing csv file in a dataframe, taking segments classified as true
+
+datadf = pd.read_csv("/home/utente/Lavoro/DE19_R3/Dati_nuovo.csv")
+signaldf = datadf.query("Y_pred_forest_data==1")
+
+#number of segments witin the same Ishower
+sizedataset = signaldf.groupby("Ishower").count()
+size = sizedataset["ID"].to_numpy()
+size = size + 1 #we need to include the injectors (not in this dataset)
+goodshowers = sizedataset.query("ID>=50").index.to_numpy() #Ishower for showers with at least 50 segments
+
+gooddf = signaldf[signaldf["Ishower"].isin(goodshowers)]
+
+#make histograms
+hsizeML = r.TH1D("hsizeML","Size of showers reconstructed by Random Forest;Nsegments", 18,0,180)
+rp.fill_hist(hsizeML,size)
+
+hIPnorm = r.TH1D("hIPnorm","Impact parameter over distance along axis;IP/#DeltaZ",30,0.,0.3)
+rp.fill_hist(hIPnorm, gooddf["Par_impact_nor"].to_numpy())
+
+hthetaprime = r.TH1D("hthetaprime","Cone angle with respect to shower start;#theta'[rad]",40,0,0.04)
+rp.fill_hist(hthetaprime,gooddf["Angolo_cono"].to_numpy())
+
+c1 = r.TCanvas()
+hsizeML.Draw()
+
+c2 = r.TCanvas()
+hIPnorm.Draw()
+
+c3 = r.TCanvas()
+hthetaprime.Draw()
+
+#write histograms to file
+histofile.cd()
+hsizeML.Write()
+hIPnorm.Write()
+hthetaprime.Write()
+
--- tutorials/showertreecomparison.C
+++ tutorials/showertreecomparison.C
@@ -1,4 +1,6 @@
-//opening a ROOT RDataFrame with a shower and computing topological variables (IP, etc. A. Iuliano 19 Feb 2021)
+/*opening a ROOT RDataFrame with a shower and computing topological variables (IP, etc. A. Iuliano 19 Feb 2021)
+adding function to compare histograms with Random Forest results 
+(saved in ROOT file with Make_histograms_data.py), launch RMcomparison*/
 using namespace ROOT;
 RVec<float> calcIP(RVec<float> dz, RVec<float> xb, RVec<float> yb, RVec<float> txb, RVec<float>tyb){
     //compute IP to vertex
@@ -39,15 +41,16 @@ ROOT::RDF::RInterface<ROOT::Detail::RDF::RJittedFilter, void> showerdataframe(TS
 void showertreecomparison(){
     //auto datadf = showerdataframe("/eos/experiment/ship/data/DESY19TB/DE19_R7/recoshower_5_December_2020/shower1.root");
     //auto simdf = showerdataframe("/eos/user/a/aiuliano/public/sims_FairShip/sim_DESY19/runs_360events/RUN7_28_December_2020/b000007/shower1.root");
+    TFile *datahistofile = new TFile("/home/utente/Lavoro/DE19_R3/b000001/recoshower_250DR_26_11/RUN3_standardrecohistos.root","RECREATE");
 
-    auto datadf = showerdataframe("/eos/experiment/ship/data/DESY19TB/DE19_R3/recoshower_250DR_26_11/shower1.root");
-    auto simdf = showerdataframe("/eos/user/a/aiuliano/public/sims_FairShip/sim_DESY19/runs_360events/RUN3_uniform_26_July_2020/b000003/recoshowers/radius1000alpha0.04/shower1.root");
+    auto datadf = showerdataframe("/home/utente/Lavoro/DE19_R3/b000001/recoshower_250DR_26_11/shower1.root");
+    auto simdf = showerdataframe("/home/utente/Simulations/RUN3_uniform_26_July_2020/b000003/recoshowers/radius1000alpha0.04/shower1.root");
 
     auto hdata_IP = datadf.Histo1D({"hdata_IP","Data;IP/#DeltaZ",30,0.,0.3},"normIP");
-    auto hdata_thetaproj = datadf.Histo1D({"hdata_thetaproj","Data;#theta'_{bt}[rad]",50,0,0.05},"thetaproj");
+    auto hdata_thetaproj = datadf.Histo1D({"hdata_thetaproj","Data;#theta'_{bt}[rad]",40,0,0.04},"thetaproj");
 
     auto hsim_IP = simdf.Histo1D({"hsim_IP","Simulation;IP/#DeltaZ",30,0.,0.3},"normIP");
-    auto hsim_thetaproj = simdf.Histo1D({"hsim_thetaproj","Simulation;#theta'_{bt}[rad]",50,0,0.05},"thetaproj");
+    auto hsim_thetaproj = simdf.Histo1D({"hsim_thetaproj","Simulation;#theta'_{bt}[rad]",40,0,0.04},"thetaproj");
 
     //drawing histograms
     TCanvas *c0 = new TCanvas();
@@ -65,4 +68,65 @@ void showertreecomparison(){
     hsim_thetaproj->DrawClone("histo");
     hdata_thetaproj->DrawClone("SAMES&&histo");
     c1->BuildLegend();
+
+    //adding full size
+    RDataFrame fulldatadf("treebranch","/home/utente/Lavoro/DE19_R3/b000001/recoshower_250DR_26_11/shower1.root");
+    auto hdata_size = fulldatadf.Histo1D({"hdata_size","Shower size;Nsegments",18,0,180},"sizeb");
+    TCanvas *csize = new TCanvas();
+    hdata_size->DrawClone();
+
+    //writing histograms to file
+    datahistofile->cd();
+    hdata_IP->Write();
+    hdata_thetaproj->Write();
+    hdata_size->Write();
+
+}
+
+void PrepareHistogram(TH1D* histo, const char * name, const char* title, int linecolor){
+
+    histo->Scale(1./histo->Integral());
+    histo->SetName(name);
+    histo->SetTitle(title);
+    histo->SetLineColor(linecolor);
+
+}
+
+void RMcomparison(){
+    TFile *standardrecofile = TFile::Open("/home/utente/Lavoro/DE19_R3/b000001/recoshower_250DR_26_11/RUN3_standardrecohistos.root");
+    TFile *RandomForestfile = TFile::Open("/home/utente/Lavoro/DE19_R3/RUN3_RandomForest_histos.root");
+    
+    //getting standard reco histograms
+    TH1D* hstandard_size = (TH1D*) standardrecofile->Get("hdata_size");
+    PrepareHistogram(hstandard_size,"hstandard_size","Standard Reconstruction;NSegments",kBlue);
+    TH1D* hstandard_normIP = (TH1D*) standardrecofile->Get("hdata_IP");
+    PrepareHistogram(hstandard_normIP,"hstandard_normIP","Standard Reconstruction;IP/#DeltaZ",kBlue);
+    TH1D* hstandard_thetaproj = (TH1D*) standardrecofile->Get("hdata_thetaproj");
+    PrepareHistogram(hstandard_thetaproj,"hstandard_thetaproj","Standard Reconstruction;#theta'",kBlue);
+
+    //getting Random Forest reco histograms
+    TH1D* hRM_size = (TH1D*) RandomForestfile->Get("hsizeML");
+    PrepareHistogram(hRM_size,"hRM_size","Random Forest Reconstruction;NSegments",kRed);
+    TH1D* hRM_normIP = (TH1D*) RandomForestfile->Get("hIPnorm");
+    PrepareHistogram(hRM_normIP,"hRM_normIP","Random Forest Reconstruction;IP/#DeltaZ",kRed);
+    TH1D* hRM_thetaproj = (TH1D*) RandomForestfile->Get("hthetaprime");
+    PrepareHistogram(hRM_thetaproj,"hRM_thetaproj","Random Forest Reconstruction;#theta'",kRed);
+
+    TCanvas *csize = new TCanvas();
+    hstandard_size->Draw("histo");
+    hRM_size->Draw("histo && SAMES");
+    csize->BuildLegend();
+    hstandard_size->SetTitle("Size of reconstructed showers;NSegments");
+
+    TCanvas *cnormIP = new TCanvas();
+    hstandard_normIP->Draw("histo");
+    hRM_normIP->Draw("histo && SAMES");
+    cnormIP->BuildLegend();
+    hstandard_normIP->SetTitle("Impact parameter over distance along axis;IP/#DeltaZ");
+
+    TCanvas *cthetaproj = new TCanvas();
+    hstandard_thetaproj->Draw("histo");
+    hRM_thetaproj->Draw("histo && SAMES");
+    cthetaproj->BuildLegend();
+    hstandard_thetaproj->SetTitle("Cone angle with respect to shower start;#theta'[rad]");
 }
