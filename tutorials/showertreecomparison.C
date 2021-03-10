f2f96187b811695bc6509e508f1eedb890b95535
Antonio Iuliano <antonio.iuliano1993@gmail.com>
2021-03-10 10:57:23 +0100

    adding 2D plots vs z and fixing Take order

--- tutorials/showertreecomparison.C
+++ tutorials/showertreecomparison.C
@@ -14,8 +14,8 @@ RVec<float> calcIP(RVec<float> dz, RVec<float> xb, RVec<float> yb, RVec<float> t
 RVec<float> calcthetaproj(RVec<float> dz, RVec<float>xb, RVec<float>yb, RVec<float> txb, RVec<float> tyb){
     RVec<float> txproj = (xb - xb[0])/dz; // projections
     RVec<float> typroj = (yb - yb[0])/dz;
-    txproj = Take(txproj, txproj.size()-1);
-    typroj = Take(typroj, typroj.size()-1);
+    txproj = Take(txproj, -(txproj.size()-1));
+    typroj = Take(typroj, -(typroj.size()-1));
 
     RVec<float> thetaproj = sqrt((txproj - txb[0]) * (txproj - txb[0]) + (typroj - tyb[0]) * (typroj - tyb[0])); //first segment is the vertex
 
@@ -29,11 +29,13 @@ ROOT::RDF::RInterface<ROOT::Detail::RDF::RJittedFilter, void> showerdataframe(TS
  auto df2 = df1.Define("IP",calcIP,{"dz","xb","yb","txb","tyb"});
  
 //Take is needed to exclude the vertex (it takes the last size -1 segments)
- auto df3 = df2.Define("normIP","Take(IP/dz,IP.size()-1)"); 
+ auto df3 = df2.Define("normIP","Take(IP/dz,-(IP.size()-1))"); 
 
  auto df4 = df3.Define("thetaproj",calcthetaproj,{"dz","xb","yb","txb","tyb"});
 
- return df4;
+ auto df5 = df4.Define("zbnofirst","Take(zb,-(zb.size()-1))");
+
+ return df5;
 
 
 }
@@ -49,8 +51,14 @@ void showertreecomparison(){
     auto hdata_IP = datadf.Histo1D({"hdata_IP","Data;IP/#DeltaZ",30,0.,0.3},"normIP");
     auto hdata_thetaproj = datadf.Histo1D({"hdata_thetaproj","Data;#theta'_{bt}[rad]",40,0,0.04},"thetaproj");
 
+    auto hdata2D_IP = datadf.Histo2D({"hdata2D_IP","Data;z[#mum];IP/#DeltaZ",40,-40000.,0.1,30,0.,0.3},"zbnofirst","normIP");
+    auto hdata2D_thetaproj = datadf.Histo2D({"hdata2D_thetaproj","Data;z[#mum];#theta'_{bt}[rad]",40,-40000.,0.1,40,0,0.04},"zbnofirst","thetaproj");
+
     auto hsim_IP = simdf.Histo1D({"hsim_IP","Simulation;IP/#DeltaZ",30,0.,0.3},"normIP");
     auto hsim_thetaproj = simdf.Histo1D({"hsim_thetaproj","Simulation;#theta'_{bt}[rad]",40,0,0.04},"thetaproj");
+
+    auto hsim2D_IP = simdf.Histo2D({"hsim2D_IP","Sim;z[#mum];IP/#DeltaZ",40,-40000.,0.1,30,0.,0.3},"zbnofirst","normIP");
+    auto hsim2D_thetaproj = simdf.Histo2D({"hsim_thetaproj","Sim;z[#mum];#theta'_{bt}[rad]",40,-40000.,0.1,40,0,0.04},"zbnofirst","thetaproj");
 
     //drawing histograms
     TCanvas *c0 = new TCanvas();
@@ -74,6 +82,20 @@ void showertreecomparison(){
     auto hdata_size = fulldatadf.Histo1D({"hdata_size","Shower size;Nsegments",18,0,180},"sizeb");
     TCanvas *csize = new TCanvas();
     hdata_size->DrawClone();
+
+    TCanvas *c2Dip = new TCanvas();
+    c2Dip->Divide(1,2);
+    c2Dip->cd(1);
+    hdata2D_IP->DrawClone("COLZ");
+    c2Dip->cd(2);
+    hsim2D_IP->DrawClone("COLZ");
+
+    TCanvas *c2Dthetaproj = new TCanvas();
+    c2Dthetaproj->Divide(1,2);
+    c2Dthetaproj->cd(1);
+    hdata2D_thetaproj->DrawClone("COLZ");
+    c2Dthetaproj->cd(2);
+    hsim2D_thetaproj->DrawClone("COLZ");
 
     //writing histograms to file
     datahistofile->cd();
