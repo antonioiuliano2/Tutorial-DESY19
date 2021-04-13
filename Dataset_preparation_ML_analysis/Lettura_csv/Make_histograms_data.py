fe259b9c2c35cd06501853817a7b5b334f48cf93
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2021-04-13 10:39:11 +0200

    making the code clearer

--- Dataset_preparation_ML_analysis/Lettura_csv/Make_histograms_data.py
+++ Dataset_preparation_ML_analysis/Lettura_csv/Make_histograms_data.py
@@ -5,10 +5,13 @@ import ROOT as r
 import root_numpy as rp
 
 minsegments = 50
-histofile = r.TFile("/home/utente/Simulations/Exercise_MariaChain/RUN5test/RUN5_RandomForest_histos.root","RECREATE")
+
+prepath = "/eos/user/a/aiuliano/public/sims_FairShip/sim_DESY19/RUN6_dataML/RandomForest/"
+
+histofile = r.TFile(prepath+"RandomForest_histos.root","RECREATE")
 #importing csv file in a dataframe, taking segments classified as true
 
-datadf = pd.read_csv("/home/utente/Simulations/Exercise_MariaChain/RUN5test/Random_Forest_withdata/Result_data.csv")
+datadf = pd.read_csv(prepath+"Result_data.csv")
 datadf = datadf[datadf["DeltaT"].isna()==False] 
 signaldf = datadf.query("Y_pred_forest_data==1")
 
@@ -16,12 +19,14 @@ signaldf = datadf.query("Y_pred_forest_data==1")
 sizedataset = signaldf.groupby("Ishower").count()
 size = sizedataset["ID"].to_numpy()
 #size = size + 1 #we need to include the injectors (not in this dataset)
-goodshowers = sizedataset.query("ID>=20").index.to_numpy() #Ishower for showers with at least 50 segments
+goodshowers = sizedataset.query("ID>={}".format(minsegments)).index.to_numpy() #Ishower for showers with at least 50 segments
+
+print("{} showers have at least {} segments".format(len(goodshowers),minsegments))
 
 gooddf = signaldf[signaldf["Ishower"].isin(goodshowers)]
 
 #make histograms
-hsizeML = r.TH1D("hsizeML","Size of showers reconstructed by Random Forest;Nsegments", 10,0,100)
+hsizeML = r.TH1D("hsizeML","Size of showers reconstructed by Random Forest;Nsegments", 20,0,200)
 rp.fill_hist(hsizeML,size)
 
 hIPnorm = r.TH1D("hIPnorm","Impact parameter over distance along axis;IP/#DeltaZ",30,0.,0.3)
